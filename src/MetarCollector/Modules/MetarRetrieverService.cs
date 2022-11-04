0c211ac9e0d64411368cf064eefeeee17507b669
Henrik Skatvedt <henrik@skatvedt.org>
2022-11-04 21:08:14 +0100

    added retry and logging for getlatestmetar delegate

--- src/MetarCollector/Modules/MetarRetrieverService.cs
+++ src/MetarCollector/Modules/MetarRetrieverService.cs
@@ -1,8 +1,13 @@
-﻿using System.Xml.Linq;
+﻿using System.Diagnostics;
+using System.Xml;
+using System.Xml.Linq;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Serilog;
 using static System.DateOnly;
 using static System.DateTime;
+using static System.TimeSpan;
 using static Microsoft.AspNetCore.WebUtilities.QueryHelpers;
 
 namespace MetarCollector.Modules;
@@ -13,7 +18,7 @@ public static class MetarRetrieverService
 {
     const string MetarServiceUrl = nameof(MetarServiceUrl);
     
-     static GetLatestMetar Setup(HttpClient httpClient) =>
+    static GetLatestMetar Setup(HttpClient httpClient) =>
         async (icao, date, ct) =>
         {
             var resp = await httpClient.GetAsync(GetUrl(), ct);
@@ -37,13 +42,43 @@ public static class MetarRetrieverService
                 });
         };
 
+     static GetLatestMetar WithRetry(this GetLatestMetar getLatestMetar) =>
+         (icao, date, ct) => 
+             Policy.Handle<Exception>()
+             .WaitAndRetryAsync(3, i => FromMilliseconds(i * 100))
+             .ExecuteAsync(() => getLatestMetar(icao, date, ct));
+
+     static GetLatestMetar WithLogging(this GetLatestMetar getLatestMetar) =>
+         async (icao, date, ct) =>
+         {
+             var logger = Log.ForContext<GetLatestMetar>(); 
+
+             try
+             {
+                logger.Debug("Getting latest metar");
+                var watch = Stopwatch.StartNew();
+                var result = await getLatestMetar(icao, date, ct);
+                logger.Debug("Finished getting metar. Elapsed: {Elapsed}", watch.Elapsed);
+                return result;
+             }
+             catch (Exception e)
+             {
+                 logger.Error(e,"Getting latest metar failed");
+                 throw;
+             }
+         };
+     
+     
     public static IServiceCollection AddMetarRetriever(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddSingleton(sp =>
             Setup(new()
-            {
-                BaseAddress = new Uri(configuration[MetarServiceUrl] ?? throw new Exception("MetarService Url is not set."))
-            }));
+                {
+                    BaseAddress = new Uri(configuration[MetarServiceUrl] ??
+                                          throw new Exception("MetarService Url is not set."))
+                })
+                .WithRetry()
+                .WithLogging());
 
         return services;
     }
